7963b22e6ea2137b8a64b308228da6fc077b792b
Allan Wang <me@allanwang.ca>
2021-09-04 19:51:23 -0700

    Add spawner

--- Assets/Simple First Person Controller/Scripts/PlayerHealth.cs
+++ Assets/Simple First Person Controller/Scripts/PlayerHealth.cs
@@ -7,22 +7,28 @@ using MLAPI.NetworkVariable;
 public class PlayerHealth : NetworkBehaviour
 {
 
-  NetworkVariableInt health = new NetworkVariableInt(100);
+  [SerializeField]
+  NetworkVariableInt health = new NetworkVariableInt(new NetworkVariableSettings { WritePermission = NetworkVariablePermission.OwnerOnly }, 100);
 
-  public int actualHealth = 100;
+  PlayerSpawner playerSpawner;
 
   // Start is called before the first frame update
   void Start()
   {
-
+    playerSpawner = GetComponent<PlayerSpawner>();
   }
 
   // Update is called once per frame
   void Update()
   {
-    actualHealth = health.Value;
+    if (IsLocalPlayer && health.Value <= 0)
+    {
+      health.Value = 100;
+      playerSpawner.Respawn();
+    }
   }
 
+  // Runs on server
   public void TakeDamage(int damage)
   {
     health.Value = Mathf.Max(0, health.Value - damage);
