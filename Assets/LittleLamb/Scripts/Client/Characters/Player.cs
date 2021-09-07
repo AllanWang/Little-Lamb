df7de278a9f6d771a9157393ae0c37a5bb21ca20
Allan Wang <me@allanwang.ca>
2021-09-07 03:55:15 -0700

    Add sprint speed

--- Assets/LittleLamb/Scripts/Client/Characters/Player.cs
+++ Assets/LittleLamb/Scripts/Client/Characters/Player.cs
@@ -14,6 +14,9 @@ namespace LittleLamb.Player
     public float movementSpeed = 5.0f;
 
     [Range(1f, 20f)]
+    public float sprintSpeed = 7.0f;
+
+    [Range(1f, 20f)]
     public float jumpSpeed = 10f;
     public float turnSpeed = 400.0f;
     private Vector3 moveDirection = Vector3.zero;
@@ -76,9 +79,11 @@ namespace LittleLamb.Player
 
       Vector3 movement = new Vector3(horizontal, 0f, vertical);
 
+      bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
       movement = Vector3.ClampMagnitude(movement, 1f);
 
-      movement *= movementSpeed;
+      movement *= isRunning ? sprintSpeed : movementSpeed;
 
       if (controller.isGrounded)
       {
@@ -103,7 +108,6 @@ namespace LittleLamb.Player
           anim.SetFloat("Velocity", 0f, 0.1f, Time.deltaTime);
           return;
         }
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
         anim.SetFloat("Velocity", isRunning ? 1f : 0.5f, 0.2f, Time.deltaTime);
       }
 
